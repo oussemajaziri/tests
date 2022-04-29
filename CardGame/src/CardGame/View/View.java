7324843b0a294cc7aadb1bfdd8ee5ac3e8f91968
oussemajaziri <102968825+oussemajaziri@users.noreply.github.com>
2022-04-29 15:57:25 +0100

    Add files via upload

---
+++ CardGame/src/CardGame/View/View.java
@@ -0,0 +1,42 @@
+package CardGame.View;
+
+import CardGame.Controller.GameController;
+
+public class View {
+
+	public void somthing() {
+
+	};
+
+	public void setController(GameController gc) {
+
+	};
+	
+	public void promptForPlayer() {
+		
+	};
+
+	public void promptForFlip() {
+		
+	};
+
+	public void promptForNewGame() {
+		
+	};
+	
+	public void showWinner(String name) {
+		
+	};
+	
+	public void showPlayerName(int size, String playerName) {
+		
+	};
+	
+	public void showFaceDownCardForPlayer(int i, String name) {
+		
+	};
+	
+	public void showCardForPlayer(int i, String name, String string,String string2) {
+		
+	};
+}
---
+++ CardGame/src/CardGame/model/PlayingCard.java
@@ -0,0 +1,36 @@
+package CardGame.model;
+
+public class PlayingCard {
+
+	private Rank rank;
+	private Suit suit;
+	private boolean faceUp;
+
+	public PlayingCard() {
+		super();
+	}
+
+	public PlayingCard(Rank rank, Suit suit) {
+		super();
+		this.rank = rank;
+		this.suit = suit;
+	}
+
+	public Rank getRank() {
+		return rank;
+	}
+
+	public Suit getSuit() {
+		return suit;
+	}
+
+	public boolean isFaceUp() {
+		return faceUp;
+	}
+
+	public boolean flip() {
+		faceUp = !faceUp;
+		return faceUp;
+	}
+
+}
